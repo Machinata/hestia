31d61d58902ec7fa699d0b18f42449da69734218
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-03 16:20:25 -0500

    done

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { logger } from "@lib/logger";
-import { yoga } from "./yoga";
+import { logger } from '@lib/logger';
+import { yoga } from './yoga';
 
 const server = Bun.serve({
-  fetch: yoga.fetch,
-  error: (error) => {
-    logger.error(error.message);
-    return new Response("", {
-      status: 500,
-      statusText: "You fucked the goose",
-    });
-  },
+	fetch: yoga.fetch,
+	error: (error) => {
+		logger.error(error.message);
+		return new Response('', {
+			status: 500,
+			statusText: 'You fucked the goose',
+		});
+	},
 });
 
 logger.info(`Server is running on: ${server.url}${yoga.graphqlEndpoint}`);
--- src/yoga/builder.ts
+++ src/yoga/builder.ts
@@ -1,29 +1,29 @@
-import type { Configuration } from "@app/config";
-import { prisma } from "@app/prisma";
-import SchemaBuilder from "@pothos/core";
+import type { Configuration } from '@app/config';
+import { prisma } from '@app/prisma';
+import SchemaBuilder from '@pothos/core';
 import PrismaPlugin, {
-  type PrismaTypesFromClient,
-} from "@pothos/plugin-prisma";
-import type { YogaInitialContext } from "graphql-yoga";
+	type PrismaTypesFromClient,
+} from '@pothos/plugin-prisma';
+import type { YogaInitialContext } from 'graphql-yoga';
 
 type Context = YogaInitialContext & {
-  config: Configuration;
+	config: Configuration;
 };
 
 export const builder = new SchemaBuilder<{
-  Context: Context;
-  PrismaTypes: PrismaTypesFromClient<typeof prisma>;
+	Context: Context;
+	PrismaTypes: PrismaTypesFromClient<typeof prisma>;
 }>({
-  plugins: [PrismaPlugin],
-  prisma: {
-    client: prisma,
-    // defaults to false, uses /// comments from prisma schema as descriptions
-    // for object types, relations and exposed fields.
-    // descriptions can be omitted by setting description to false
-    exposeDescriptions: false,
-    // use where clause from prismaRelatedConnection for totalCount (defaults to true)
-    filterConnectionTotalCount: true,
-    // warn when not using a query parameter correctly
-    onUnusedQuery: process.env.NODE_ENV === "production" ? null : "warn",
-  },
+	plugins: [PrismaPlugin],
+	prisma: {
+		client: prisma,
+		// defaults to false, uses /// comments from prisma schema as descriptions
+		// for object types, relations and exposed fields.
+		// descriptions can be omitted by setting description to false
+		exposeDescriptions: false,
+		// use where clause from prismaRelatedConnection for totalCount (defaults to true)
+		filterConnectionTotalCount: true,
+		// warn when not using a query parameter correctly
+		onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn',
+	},
 });
