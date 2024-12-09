a97d9313a7fffa00fa6919797f44cc0ce36ede7c
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-08 23:48:20 -0500

    basic navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let { title }: { title: string } = $props();
+</script>
+
+<header>
+	<div class="navbar">
+		<div>
+			<h2>Hestia</h2>
+		</div>
+		<div>
+			<h1>{title}</h1>
+		</div>
+		<div>
+			<p>Welcome!</p>
+		</div>
+	</div>
+</header>
+
+<style>
+	.navbar {
+		@apply flex items-center justify-between border-b-slate-200 bg-slate-100 px-6 py-2 drop-shadow;
+	}
+
+	.navbar h1 {
+		@apply text-2xl;
+	}
+
+	.navbar h2 {
+		@apply text-xl;
+	}
+</style>
