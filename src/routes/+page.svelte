05faa76d7d028de3b207ee556c908d49fcfd4315
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-10 09:45:40 -0500

    font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import Loader from '$lib/components/Loader.svelte';
 
 	$effect(() => {
 		const id = setTimeout(() => {
-			window.location.href = '/app';
+			goto('/app');
 		}, 1500);
 		return () => {
 			clearTimeout(id);
@@ -21,6 +22,6 @@
 		@apply flex h-screen w-screen flex-col items-center justify-center gap-6 bg-slate-100;
 	}
 	.site-loader h1 {
-		@apply text-4xl;
+		@apply font-display text-4xl;
 	}
 </style>
