79548434482eca95c7df3c5068c01ad6bcb126a7
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-10 10:00:35 -0500

    move to lib

---
+++ src/lib/yoga/index.ts
@@ -0,0 +1,22 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import { createSchema, createYoga } from 'graphql-yoga';
+
+export const Yoga = createYoga<RequestEvent>({
+	schema: createSchema({
+		typeDefs: `
+			type Query {
+				hello: String
+			}
+		`,
+		resolvers: {
+			Query: {
+				hello: () => 'SvelteKit - GraphQL Yoga'
+			}
+		}
+	}),
+	// Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
+	graphqlEndpoint: '/api/graphql',
+
+	// Needed to let Yoga use sveltekit's Response object
+	fetchAPI: { Response }
+});
--- src/routes/api/graphql/+server.ts
+++ src/routes/api/graphql/+server.ts
@@ -1,24 +1,3 @@
-import { createSchema, createYoga } from 'graphql-yoga';
-import type { RequestEvent } from '@sveltejs/kit';
+import { Yoga } from '$lib/yoga';
 
-const yogaApp = createYoga<RequestEvent>({
-	schema: createSchema({
-		typeDefs: `
-			type Query {
-				hello: String
-			}
-		`,
-		resolvers: {
-			Query: {
-				hello: () => 'SvelteKit - GraphQL Yoga'
-			}
-		}
-	}),
-	// Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-	graphqlEndpoint: '/api/graphql',
-
-	// Needed to let Yoga use sveltekit's Response object
-	fetchAPI: { Response }
-});
-
-export { yogaApp as GET, yogaApp as POST };
+export { Yoga as GET, Yoga as POST };
