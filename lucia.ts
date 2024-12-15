cc08ab0e48c9474ef19e498a2b01036e9d929ed8
Dan Mihailescu <as42554525@yahoo.ca>
2024-12-15 12:05:54 -0500

    fixed adapter

--- lucia.ts
+++ lucia.ts
@@ -1,12 +1,22 @@
 import { Lucia } from "lucia";
+//import { dev } from "$app/env";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 const adapter = new PrismaAdapter(client.session, client.user)
 // expect error (see next section)
-export const auth = new Lucia({
-
+export const auth = new Lucia(adapter, {
+    sessionCookie: {
+        attributes: {
+           // secure: !dev
+        }
+    },
+    getUserAttributes:  (attributes)=>{
+        return {
+            email: attributes.email
+        }
+    }
 });
 
 export type Auth = typeof auth;
