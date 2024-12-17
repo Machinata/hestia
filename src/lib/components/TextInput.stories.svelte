dba9e9f05dafecc98ae59e499054ba1804ab15b8
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-17 17:14:03 -0500

    rename Input to TextInput and use daisy

---
+++ src/lib/components/TextInput.stories.svelte
@@ -0,0 +1,13 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import TextInput from './TextInput.svelte';
+
+	const { Story } = defineMeta({
+		title: 'TextInput',
+		component: TextInput,
+		tags: ['autodocs'],
+		argTypes: {}
+	});
+</script>
+
+<Story name="Text" args={{ label: 'Text', name: 'text' }} />
