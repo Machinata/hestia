39c96ac88ae98cef7a54f021a0f2b231ba3ab693
Dan Mihailescu <as42554525@yahoo.ca>
2024-12-18 12:07:15 -0500

    fixed event param

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import { prisma } from '$lib/server/prisma';
 export async function load(event) {
-	console.log(event.userId);
-	const userId = event.userId;
+	console.log(event.cookies.get('user'));
+	const userId = event.cookies.get('user');
 	if (!userId) {
 		return {
 			authenticated: false
