a74080b1c123800125bbc885755ba5604bc1460c
DanMihailescu <as42554525@yahoo.ca>
2024-12-18 14:33:59 -0500

    Update +page.server.ts
    Signed-off-by: DanMihailescu <as42554525@yahoo.ca>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,5 @@
 import { prisma } from '$lib/server/prisma';
 export async function load(event) {
-	console.log(event.cookies.get('user'));
 	const userId = event.cookies.get('user');
 	if (!userId) {
 		return {
