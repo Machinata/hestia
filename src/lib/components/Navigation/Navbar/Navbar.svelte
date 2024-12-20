abc30a2a03ef6f1a6a34a616618983ed1c410366
Baobeld <benjaminpalko@hotmail.com>
2024-12-19 22:24:44 -0500

    Greet user (#21)
    * ignore underscores
    
    * great user and redirect to login on /app
    
    * format
    
    * pass user as property of data object

--- src/lib/components/Navigation/Navbar/Navbar.svelte
+++ src/lib/components/Navigation/Navbar/Navbar.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	let { title }: { title: string } = $props();
+	let { title, username }: { title: string; username: string } = $props();
+
+	let greeting = $derived(`Welcome ${username}!`);
 </script>
 
 <header class="navbar justify-between bg-base-200 px-4">
 	<h2 class="prose prose-xl">Hestia</h2>
 	<h1 class="prose prose-2xl">{title}</h1>
-	<p class="prose prose-lg">Welcome!</p>
+	<p class="prose prose-lg">{greeting}</p>
 </header>
 
 <style>
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { Navbar } from '$lib/components/Navigation';
+	import type { User } from '@prisma/client';
+	import type { Snippet } from 'svelte';
 
-	let { children } = $props();
+	type Props = {
+		children: Snippet;
+		data: {
+			user: Omit<User, 'password'>;
+		};
+	};
+
+	let { children, data }: Props = $props();
 </script>
 
-<Navbar title="Svelte" />
+<Navbar title="Svelte" username={data.user.name} />
 {@render children()}
