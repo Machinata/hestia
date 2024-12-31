9451f5ceab3e3d6b9d6783db93a014aa5be91153
Benjamin Palko <benjaminpalko@hotmail.com>
2024-12-30 23:48:16 -0500

    add twilio FROM number

--- src/lib/server/twilio/twilio.config.ts
+++ src/lib/server/twilio/twilio.config.ts
@@ -1,16 +1,19 @@
+import { PhoneRegex } from '$lib/regex';
 import { logger } from '$lib/server/logger';
 import { z } from 'zod';
 
 export interface TwilioConfiguration {
 	twilio_account_sid: string;
 	twilio_auth_token: string;
+	twilio_phone_number: string;
 }
 
 const LoadConfig = (): TwilioConfiguration => {
 	const { success, data, error } = z
 		.object({
 			VITE_TWILIO_ACCOUNT_SID: z.string(),
-			VITE_TWILIO_AUTH_TOKEN: z.string()
+			VITE_TWILIO_AUTH_TOKEN: z.string(),
+			VITE_TWILIO_PHONE_NUMBER: z.string().regex(PhoneRegex),
 		})
 		.safeParse(import.meta.env);
 
@@ -20,7 +23,8 @@ const LoadConfig = (): TwilioConfiguration => {
 
 	return {
 		twilio_account_sid: data!.VITE_TWILIO_ACCOUNT_SID,
-		twilio_auth_token: data!.VITE_TWILIO_AUTH_TOKEN
+		twilio_auth_token: data!.VITE_TWILIO_AUTH_TOKEN,
+		twilio_phone_number: data!.VITE_TWILIO_PHONE_NUMBER,
 	};
 };
 
