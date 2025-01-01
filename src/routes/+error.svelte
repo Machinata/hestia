277799cb0030942e3545434bbbeae27fff94e03c
Dan Mihailescu <as42554525@yahoo.ca>
2025-01-01 09:13:29 -0500

    fixed error code display

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { error } from '@sveltejs/kit';
-
-	let code = 400;
-	switch (page.error?.message) {
-		case 'Not Found': {
-			code = 404;
-			break;
-		}
-	}
 </script>
 
 <main class="flex h-screen w-full flex-col items-center justify-center bg-[#1d222a]">
 	<h1 class="text-9xl font-extrabold tracking-widest text-white">
-		{code}
+		{page.status}
 	</h1>
 	<h1 class="rounded bg-[#FF6A3D] px-2 text-2xl font-bold text-black">
 		{page.error?.message}
