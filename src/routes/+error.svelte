5ca87f2dd51f34cb6e5afd3f64a075da158a052d
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-01 11:06:34 -0500

    Reformat error page
    GET EM OUTTA HERE!
    
    format

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import { messages } from '$lib/i18n';
 </script>
 
-<main class="flex h-screen w-full flex-col items-center justify-center bg-[#1d222a]">
-	<h1 class="text-9xl font-extrabold tracking-widest text-white">
-		{page.status}
-	</h1>
-	<h1 class="rounded bg-[#FF6A3D] px-2 text-2xl font-bold text-black">
-		{page.error?.message}
-	</h1>
-
-	<button class="mt-5">
-		<a
-			class="group relative inline-block text-sm font-medium text-[#FF6A3D] focus:outline-none focus:ring active:text-orange-500"
-			href="/"
-		>
-			<span class="relative block border border-current bg-[#1d222a] px-8 py-3">
-				Go Home
-			</span>
-		</a>
-	</button>
+<main class="flex h-full w-full flex-col items-center justify-center gap-8">
+	<div class="flex items-center gap-2">
+		<h1 class="text-5xl font-bold tracking-widest text-white">
+			{page.status}
+		</h1>
+		<div class="divider divider-horizontal"></div>
+		<h1 class="text-2xl font-bold">
+			{page.error?.message}
+		</h1>
+	</div>
+	<button onclick={() => goto('/')} class="btn btn-outline btn-neutral btn-wide"
+		>{messages.error_page_go_home()}</button
+	>
 </main>
