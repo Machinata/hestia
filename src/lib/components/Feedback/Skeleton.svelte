b375a36d6b30d8c6bddc069b4e22c38c22c889fe
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-03 14:42:19 -0500

    Skeleton component

---
+++ src/lib/components/Feedback/Skeleton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
+
+	type Props = {
+		children?: Snippet;
+	} & SvelteHTMLElements['div'];
+	let { children, class: className, ...props }: Props = $props();
+</script>
+
+<div {...props} class={twMerge('skeleton', className)}>{@render children?.()}</div>
--- src/lib/components/Feedback/index.ts
+++ src/lib/components/Feedback/index.ts
@@ -2,4 +2,5 @@ export { default as Alert } from './Alert.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as Loading } from './Loading.svelte';
 export { default as Progress } from './Progress.svelte';
+export { default as Skeleton } from './Skeleton.svelte';
 export { default as Tooltip } from './Tooltip.svelte';
