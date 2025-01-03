04b315f2c6855ebed8a554438211720d86afd375
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-03 14:41:20 -0500

    Tooltip component

---
+++ src/lib/components/Feedback/Tooltip.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { DaisyColor } from '$lib/types';
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
+
+	type Props = {
+		color?: Exclude<DaisyColor, 'neutral' | 'ghost'>;
+		open?: boolean;
+		position?: 'top' | 'bottom' | 'left' | 'right';
+		tip?: string;
+	} & SvelteHTMLElements['div'];
+	let { children, class: className, color, open, position, tip, ...props }: Props = $props();
+</script>
+
+<div
+	class={twMerge('tooltip', className)}
+	class:tooltip-primary={color === 'primary'}
+	class:tooltip-secondary={color === 'secondary'}
+	class:tooltip-accent={color === 'accent'}
+	class:tooltip-info={color === 'info'}
+	class:tooltip-success={color === 'success'}
+	class:tooltip-warning={color === 'warning'}
+	class:tooltip-error={color === 'error'}
+	class:tooltip-open={open}
+	class:tooltip-top={position === 'top'}
+	class:tooltip-bottom={position === 'bottom'}
+	class:tooltip-left={position === 'left'}
+	class:tooltip-right={position === 'right'}
+	data-tip={tip}
+	{...props}
+>
+	{@render children?.()}
+</div>
--- src/lib/components/Feedback/index.ts
+++ src/lib/components/Feedback/index.ts
@@ -2,3 +2,4 @@ export { default as Alert } from './Alert.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as Loading } from './Loading.svelte';
 export { default as Progress } from './Progress.svelte';
+export { default as Tooltip } from './Tooltip.svelte';
