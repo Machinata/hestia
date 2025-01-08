a5ccc837f7c3283e0781fa3f6cec5f87c706af42
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-08 16:14:24 -0500

    add encryption/decryption + env vars

--- scripts/validate-env.ts
+++ scripts/validate-env.ts
@@ -7,6 +7,9 @@ const ValidateEnvironment = () => {
 			TWILIO_ACCOUNT_SID: z.string().min(1),
 			TWILIO_AUTH_TOKEN: z.string().min(1),
 			TWILIO_PHONE_NUMBER: z.string().regex(PhoneRegex),
+			SECRETS_PASSWORD: z.string().length(32),
+			SECRETS_SALT: z.string().min(16),
+			SECRETS_IV_POSITION: z.number().positive(),
 		})
 		.safeParse(process.env);
 
