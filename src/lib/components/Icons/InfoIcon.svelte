64f1f11133ee7efff5dac6c4c19d9394dc912117
Baobeld <benjaminpalko@hotmail.com>
2025-01-08 10:13:07 -0500

    Chore svelte lucide (#48)
    * add lucide
    
    * move components to lucide
    
    * remove flaticons

--- src/lib/components/Icons/InfoIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { SvelteHTMLElements } from 'svelte/elements';
-	import { twMerge } from 'tailwind-merge';
-
-	let { class: className, ...props }: SvelteHTMLElements['i'] = $props();
-</script>
-
-<i {...props} class={twMerge(className, 'fi fi-sr-info')}></i>
