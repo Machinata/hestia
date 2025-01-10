4b00961a322da51fd9c1a49bebb97f1b3f73931e
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-09 22:20:13 -0500

    remove TWILIO env vars

--- scripts/validate-env.ts
+++ scripts/validate-env.ts
@@ -1,12 +1,8 @@
 import { z } from 'zod';
-import { PhoneRegex } from '../src/lib/regex/phone';
 
 const ValidateEnvironment = () => {
 	const { success, error } = z
 		.object({
-			TWILIO_ACCOUNT_SID: z.string().min(1),
-			TWILIO_AUTH_TOKEN: z.string().min(1),
-			TWILIO_PHONE_NUMBER: z.string().regex(PhoneRegex),
 			SECRETS_PASSWORD: z.string().length(32),
 			SECRETS_SALT: z.string().min(16),
 			SECRETS_IV_POSITION: z.string().transform((val, ctx) => {
