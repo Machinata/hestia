0728be48b5b698eb359506a85c406445031be62e
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-09 22:20:49 -0500

    add menu dropdown on navbar
    add messages to navbar

--- src/lib/components/Navigation/Navbar/Navbar.stories.svelte
+++ src/lib/components/Navigation/Navbar/Navbar.stories.svelte
@@ -8,4 +8,20 @@
 	});
 </script>
 
-<Story name="Default" args={{ title: 'Storybook' }} />
+<Story name="Default">
+	<Navbar>
+		{#snippet start()}
+			<button class="btn">Home</button>
+		{/snippet}
+		{#snippet center()}
+			<h2 class="text-xl">Title</h2>
+		{/snippet}
+		{#snippet end()}
+			<div class="avatar placeholder">
+				<div class="w-8 rounded-full bg-neutral text-neutral-content">
+					<span class="text-xs">UI</span>
+				</div>
+			</div>
+		{/snippet}
+	</Navbar>
+</Story>
