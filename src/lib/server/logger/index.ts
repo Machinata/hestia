5d1eaddee8f3499762ecd8afbc2e22c43236ec3f
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-16 19:55:58 -0500

    add pino pretty transport

--- src/lib/server/logger/index.ts
+++ src/lib/server/logger/index.ts
@@ -1,7 +1,18 @@
+import { NODE_ENV } from '$env/static/private';
 import { type YogaLogger } from 'graphql-yoga';
 import pino from 'pino';
 
-export const logger = pino();
+export const logger = pino({
+	//	Only use pino-pretty when NOT production
+	...(NODE_ENV !== 'production' && {
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				colorize: true,
+			},
+		},
+	}),
+});
 
 export const yogaLogger: YogaLogger = {
 	debug(...args) {
