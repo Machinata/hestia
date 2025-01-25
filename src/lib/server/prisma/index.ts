961a09c27cc894cfb60b8dc33cee0b0ff1024a8e
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-25 11:50:26 -0500

    driver adapters

--- src/lib/server/prisma/index.ts
+++ src/lib/server/prisma/index.ts
@@ -1,3 +1,10 @@
+import { DATABASE_URL } from '$env/static/private';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import pg from 'pg';
 
-export const prisma = new PrismaClient();
+const connectionString = `${DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
