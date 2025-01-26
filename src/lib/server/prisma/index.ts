56ef83778053402b20389aba491946f19c433bdf
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-25 23:03:31 -0500

    add logs

--- src/lib/server/prisma/index.ts
+++ src/lib/server/prisma/index.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { logger } from '../logger';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+	log: [{ emit: 'event', level: 'query' }],
+});
+
+prisma.$on('query', (event) => {
+	logger.debug(`Query [${event.duration}ms]: ${event.query}`);
+});
