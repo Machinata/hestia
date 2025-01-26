086290e76c11c6772a6f38bfd07782e075815a39
Benjamin Palko <benjaminpalko@hotmail.com>
2025-01-25 22:37:08 -0500

    logging on prisma

--- src/lib/server/prisma/index.ts
+++ src/lib/server/prisma/index.ts
@@ -2,9 +2,14 @@ import { DATABASE_URL } from '$env/static/private';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import pg from 'pg';
+import { logger } from '../logger';
 
 const connectionString = `${DATABASE_URL}`;
 
 const pool = new pg.Pool({ connectionString });
 const adapter = new PrismaPg(pool);
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter, log: [{ emit: 'event', level: 'query' }] });
+
+prisma.$on('query', (event) => {
+	logger.debug(`Query [${event.duration}ms]: ${event.query}`);
+});
