8006d523c78407fb3807c445e22207322066cfd1
Baobeld <benjaminpalko@hotmail.com>
2025-01-26 19:48:26 -0500

    Netlify fix env vars and Deploy prisma (#69)
    * add logs
    
    * info
    
    * lol
    
    * direct url

--- src/lib/server/prisma/index.ts
+++ src/lib/server/prisma/index.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { logger } from '../logger';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+	log: [
+		{ emit: 'event', level: 'query' },
+		{ emit: 'event', level: 'info' },
+	],
+});
+
+prisma.$on('query', (event) => {
+	logger.debug(`Query [${event.duration}ms]: ${event.query}`);
+});
+
+prisma.$on('info', (event) => {
+	logger.info(event.message);
+});
