25ef06cfbf057aad7a79ebb57be4b307b7bb798a
Benjamin Palko <benjaminpalko@hotmail.com>
2025-02-03 17:36:09 -0500

    create resident table

---
+++ prisma/migrations/20250203223348_add_residents/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Resident" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Resident_pkey" PRIMARY KEY ("id")
+);
